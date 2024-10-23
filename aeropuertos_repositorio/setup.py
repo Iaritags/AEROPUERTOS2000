ce014a574644cfa0d68722106905c01e822ce0f9
Diego <casescas@gmail.com>
2024-10-23 00:34:35 -0300

    Se incorpora estructura de proyecto con Cookiecutter

---
+++ aeropuertos_repositorio/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='aeropuertos_repositorio',
+    packages=find_packages(),
+    description='Sistema de análisis de datos con powerBI',
+    author='Equipo PP1 Aeropuertos2000 2C 2024',
+    version='0.0.1'
+)
